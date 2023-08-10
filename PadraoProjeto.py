f03931c1f084a02a4b710a83e4e505d8b492cf14
ArthurSampaio13 <141786964+ArthurSampaio13@users.noreply.github.com>
2023-08-10 20:31:50 -0300

    Update PadraoProjeto.py
    adições

--- PadraoProjeto.py
+++ PadraoProjeto.py
@@ -1,23 +1,25 @@
 # Código para ser seguido nos projetos 
 
-# Importação das bibliotecas: ==========================================================================================================================================
+# Importação das bibliotecas e outras necessidades: ==========================================================================================================================================
 
 from tkinter import *
 
 # variveis globais: =====================================================================================================================================================
 
-CORBRANCA = "#F2F2F0"
-CORZINZACLARO = "#A5A6A4"
-CORZINZAESCURO = "#737373"
-CORLARANJACLARO = "#BF8450"
-CORLARANJAESCURO = "#BF4C0A"
+COR_BRANCA = "#F2F2F0"
+COR_CINZA_CLARO = "#A5A6A4"
+COR_CINZA_ESCURO = "#737373"
+COR_LARANJA_CLARO = "#BF8450"
+COR_LARANJA_ESCURO = "#BF4C0A"
+fonte_titulo = ('mulish', 22, 'bold')
+fonte_conteudo = ('monospace', 16)
 
 # Inicalização da janela: ===============================================================================================================================================
 
 janela = Tk()
 janela.title("Nome da aplicação")
 janela.geometry('1366x768')
-janela.config(background=CORBRANCA)
+janela.config(background=COR_BRANCA)
 janela.resizable(width=FALSE, height=FALSE)
     
 # Continuação do codigo: ================================================================================================================================================
