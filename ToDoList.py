de2865c7b833021ddb274fa37816d0cda0c39aaf
Jeova BL <jeovab115@gmail.com>
2023-08-11 15:36:50 -0300

    janela adicionada todolist

--- ToDoList.py
+++ ToDoList.py
@@ -1,1 +1,39 @@
-#Jeovhys
+# Importação das bibliotecas e outras necessidades: ==========================================================================================================================================
+
+from tkinter import *
+
+# variveis globais: =====================================================================================================================================================
+
+COR_BRANCA = "#F2F2F0"
+COR_CINZA_CLARO = "#A5A6A4"
+COR_CINZA_ESCURO = "#737373"
+COR_LARANJA_CLARO = "#BF8450"
+COR_LARANJA_ESCURO = "#BF4C0A"
+fonte_titulo = ('mulish', 22, 'bold')
+fonte_conteudo = ('monospace', 16)
+
+# Inicalização da janela: ==============================================================================================================================================
+
+janela = Tk()
+janela.title("To-Do List")
+janela.geometry('1280x700')
+janela.config(background=COR_CINZA_ESCURO)
+janela.resizable(width=FALSE, height=FALSE)
+    
+# Desenvolvimento da janela:============================================================================================================================================
+
+# Parte de cima da tela
+top_frame = ...
+
+
+
+
+
+
+
+
+
+
+# Ciclo da janela para ela ir atualizando:==============================================================================================================================
+
+janela.mainloop()
