0574b108408728d0e5205760976f1ea4b62a4b6a
PierreOF <marcos.pierre@upe.br>
2023-08-12 18:06:26 -0300

    Feature adicionando botões funcionais

---
+++ src/FuncoesTelaPrincipal.py
@@ -0,0 +1,10 @@
+
+from tkinter import *
+import tkinter as tk
+import webbrowser
+
+# Função para abrir a pagina do código no github
+def abrir_navegador():
+    nova_url = "https://github.com/Jeova-1704/Projeto-programacao-1"
+    webbrowser.open(nova_url)
+
