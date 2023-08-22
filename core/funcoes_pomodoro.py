e7d8c71342250b1a1f4d094e5f428727209f22b3
Ana Oliveira <beatriz.oliveirac@upe.br>
2023-08-22 11:28:07 -0300

    Update pomodoro - fase inicial

---
+++ core/funcoes_pomodoro.py
@@ -0,0 +1,31 @@
+import time
+import os
+
+foco = 25
+pausa = 5
+
+
+def conversao(t):
+    pass
+    # de min para segundos:
+    """return t * 60"""
+
+
+def pomodoro(foco, pausa):
+    pass
+    """f = conversao(foco)
+    p = conversao(pausa)
+    temporizador(f, "FOCO")
+    os.system("cls")  # clear do windows
+    temporizador(p, "PAUSA")
+    os.system("cls")"""
+
+def click_icon(tempo, label):
+    pass
+'''configuração em uma nova abinha do tempo de pausa, tempo de foco'''
+
+def temporizador(tempo, label):
+    pass
+
+
+#pomodoro(foco, pausa)
